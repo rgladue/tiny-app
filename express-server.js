dc1b4d939b94ef3769f1984fe08767ac424b01bf
robert <robgladue@hotmail.com>
2021-12-07 01:14:12 +0000

    added generate random short URL

--- express-server.js
+++ express-server.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+let randomArr = [];
+for (let i = 0; i < 1; i++) {
+  randomArr.push(Math.random().toString(24).slice(8))
+}return randomArr[0];
+}
+console.log(generateRandomString());
 
 app.set("view engine", "ejs");
 
@@ -18,6 +28,10 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
@@ -30,6 +44,11 @@ app.get("/urls", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
